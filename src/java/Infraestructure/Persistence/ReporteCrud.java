5e6c54cebab680e0d4b2331abbf27091540f43a4
pc <pc@Yesid>
2024-11-20 18:17:53 -0500

    Actualizacion de clase Reporte Crud

--- src/java/Infraestructure/Persistence/ReporteCrud.java
+++ src/java/Infraestructure/Persistence/ReporteCrud.java
@@ -3,8 +3,33 @@ package Infraestructure.Persistence;
 
 /**
  *
- * @Yesid
+ * @Yesid Martinez
  */
+import Domain.Model.Reporte;
+import Infraestructure.Database.ConnectionDbMysql;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReporteCrud {
-    
+    public List<Reporte>getAllReportes(){
+    List<Reporte> reportes=new ArrayList<>();
+    String query= "SELECT * FROM reporte";
+        try {
+            Connection conn =ConnectionDbMysql.getConnection();
+            
+            Statement stmt = conn.createStatement();
+            ResultSet rs= stmt.executeQuery(query);
+            
+            while (rs.next()){
+                //reportes.add(0, "");
+               rs.getString(query);
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    return reportes;
+    }
 }
